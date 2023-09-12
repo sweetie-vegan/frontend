b4b0511633efda78d65f2ff27fcb334f72ada2c5
Hyewon Lee <hwlee0915@gmail.com>
2023-09-12 14:51:12 -0300

    build react app

---
+++ sweetie-vegan/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
